a176ecc81426bcd4fa50a95f284f54b1fb7100a1
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-05-18 15:27:54 -0400

    added server.js with initial code

---
+++ server.js
@@ -0,0 +1,11 @@
+// include express
+const express = require("express");
+const app = express();
+
+//defining port
+const PORT = process.env.PORT || 3001;
+
+// middleware for parsing JSON and urlencode form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));

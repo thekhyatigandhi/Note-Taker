5ad11ddc79fae5f0b75ad58fc1c799e44b5330c5
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-05-20 15:45:45 -0400

    added code to api routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-let notes = require('../db/db.json');
-const { v4: uuidv4 } = require('uuid');
+const fs = require("fs");
+let notes = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");
 console.log(uuidv4());
 
+// GET /api/notes (return db.json)
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});

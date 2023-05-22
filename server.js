92e836e48a2d7dd93b5ee23770ee340b1d92cd42
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-05-22 00:08:12 -0400

    added commands in server.js

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-// include express
+// including express
 const express = require("express");
 const app = express();
 
-//defining port
+// defining the port - local port 3001 and port for heroku
 const PORT = process.env.PORT || 3001;
 
-// middleware for parsing JSON and urlencode form data
+// applying middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// html/api routers
+// require html/api routers
+const htmlRouter = require("./routes/html.js");
+const apiRouter = require("./routes/api.js");
 
 // link routes to path
+app.use(htmlRouter);
+app.use("/api", apiRouter);
 
 // port check
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));

c0d9d32928b82f970762c6c0a22da50268a6b656
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-05-22 00:06:15 -0400

    made changes in html.js

--- routes/html.js
+++ routes/html.js
@@ -5,12 +5,12 @@ const path = require("path");
 // node:path module provides utilities for working with file and directory paths
 const path = require("path");
 
-// GET * (return index.html)
+// GET method to return index.html
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/../routes/public/index.html"));
 });
 
-// GET /notes (return notes.html)
+// GET method to return notes.html
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/../public/notes.html"));
 });

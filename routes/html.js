7a191a13cf41f10ee552bdadb78a5170b553013a
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-05-22 00:05:10 -0400

    added code to html.js

--- routes/html.js
+++ routes/html.js
@@ -2,3 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const path = require("path");
+// node:path module provides utilities for working with file and directory paths
+const path = require("path");
+
+// GET * (return index.html)
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../routes/public/index.html"));
+});
+
+// GET /notes (return notes.html)
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../public/notes.html"));
+});
+
+module.exports = router;
